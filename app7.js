7fd1b89f236819fd156f58b466bcda2700e971a6
Ashutosh Adhikari <veni_vidi.vici@outlook.com>
2025-06-11 17:58:43 +0530

    Router and Filters - Task II

--- app7.js
+++ app7.js
@@ -1,12 +1,11 @@
 const express = require("express")
 const app = express()
-const productRouter = require("./routes/products")
-const categoriesRouter = require("./routes/categories")
+
+const booksRouter = require("./routes/books")
 const PORT = 4000
 
 
-app.use("/products", productRouter)
-app.use("/categories", categoriesRouter)
+app.use("/books", booksRouter)
 
 
 
