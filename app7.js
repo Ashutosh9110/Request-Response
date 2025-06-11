0a9dede089c1f08dad2d5a26cbb112c3b0899192
Ashutosh Adhikari <veni_vidi.vici@outlook.com>
2025-06-11 16:47:07 +0530

    Challenge- Create a Server with Middleware for Logging

--- app7.js
+++ app7.js
@@ -1,15 +1,30 @@
 const express = require("express")
 const app = express()
-const PORT = 3000
+const PORT = 4000
 
 
-app.get("/welcome/:name", (req, res) => {
-  const name = req.params.name
-  const role = req.query.role
-
-  res.send(`Welcome ${name}, your role is ${role}`)
-
+app.use((req, res, next) => {
+  console.log(`${req.method} request made to ${req.url}`);
+  next()
 })
+
+app.get("/products", (req, res) => {
+  res.send("Here is the list of all products.")
+})
+
+app.post("/products", (req, res) => {
+  res.send("A new product has been added.")
+})
+
+app.get("/categories", (req, res) => {
+  res.send("Here is the list of all categories.")
+})
+
+app.post("/categories", (req, res) => {
+  res.send("A new category has been created.")
+})
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is running at ${PORT}`);
