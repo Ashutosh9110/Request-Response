598e35e777147ce7097932b1bcddc31078808716
Ashutosh Adhikari <veni_vidi.vici@outlook.com>
2025-06-11 17:51:12 +0530

    Router and Filters

--- app7.js
+++ app7.js
@@ -1,28 +1,12 @@
 const express = require("express")
 const app = express()
+const productRouter = require("./routes/products")
+const categoriesRouter = require("./routes/categories")
 const PORT = 4000
 
 
-app.use((req, res, next) => {
-  console.log(`${req.method} request made to ${req.url}`);
-  next()
-})
-
-app.get("/products", (req, res) => {
-  res.send("Here is the list of all products.")
-})
-
-app.post("/products", (req, res) => {
-  res.send("A new product has been added.")
-})
-
-app.get("/categories", (req, res) => {
-  res.send("Here is the list of all categories.")
-})
-
-app.post("/categories", (req, res) => {
-  res.send("A new category has been created.")
-})
+app.use("/products", productRouter)
+app.use("/categories", categoriesRouter)
 
 
 
